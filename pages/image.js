ca0be7a9ebde11084a1ca30c180e5f084a675d2f
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-05-12 22:00:51 +0100

    add styling

--- pages/image.js
+++ pages/image.js
@@ -1,34 +1,26 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function Image() {
+export default function ImagePage() {
   const router = useRouter();
   const { data } = router.query;
-
-  const dataJSON = JSON.parse(data);
-
-  const image = dataJSON;
+  const image = JSON.parse(data);
   const imageSrc = image.webformatURL;
   const imageAlt = image.tags;
   const imageHeight = image.webformatHeight;
   const imageWidth = image.webformatWidth;
-  const imagePage = image.pageURL;
-  const imageUser = image.user;
-  const imageUserImage = image.userImageURL;
-  const imageUserProfile = image.userProfileURL;
 
   return (
-    <div>
+    <main>
       <h1>{imageAlt}</h1>
-      <img src={imageSrc} alt={imageAlt} height={imageHeight} width={imageWidth} />
-      <p>{imagePage}</p>
-      <p>{imageUser}</p>
-      <img src={imageUserImage} alt={imageUser} />
-      <p>{imageUserProfile}</p>
+      <Image src={imageSrc} alt={imageAlt} height={imageHeight} width={imageWidth} />
 
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-    </div>
+      <div className="link">
+        <Link href="/">
+          <a>Go Back Home</a>
+        </Link>
+      </div>
+    </main>
   );
 }
